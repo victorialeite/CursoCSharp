da250c756d327ce4ded4bbc03c1139294bf537d0
Victoria Leite <victorialeitemarques8@gmail.com>
2018-08-22 23:30:00 -0300

    First Commit

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Inferencia
+    {
+        public static void Executar()
+        {
+            var nome = "Leonardo";
+            Console.WriteLine(nome);
+
+
+        }
+    }
+}
